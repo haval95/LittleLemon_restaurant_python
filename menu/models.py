97ebb0cdc8eb046ceff4af06deceec8215e21aab
haval95 <havalrezan22@gmail.com>
2023-05-14 00:25:30 +0300

    FEAT: menu models registerd and migrated to db

--- menu/models.py
+++ menu/models.py
@@ -12,8 +12,8 @@ class Menu_Item(models.Model):
     item_name = models.CharField(max_length=100)
     item_ingridiants = models.CharField(max_length=500)
     item_price = models.IntegerField()
-    category_id = models.name = models.ForeignKey(
-        Category, related_name="category_name", on_delete=models.PROTECT, default=None
+    category_id = models.ForeignKey(
+        Category, related_name="category", on_delete=models.PROTECT, default=None
     )
 
     def __str__(self) -> str:
