f3651e90d58363fcdb28518a787441ac284b5722
haval95 <havalrezan22@gmail.com>
2023-05-13 22:44:16 +0300

    FEAT: index page desing done

--- static/js/navbar.js
+++ static/js/navbar.js
@@ -8,12 +8,12 @@ const mobileMenu = document.getElementById('mobile-menu');
 
         window.addEventListener("scroll", () => {
             if (window.scrollY > 100) {
-                navbar.classList.remove("bg-transparent");
+                navbar.classList.remove("navbg");
                 navbar.classList.add("bg-white");
                 navbar.classList.add("shadow");
             } else {
                 navbar.classList.remove("bg-white");
                 navbar.classList.remove("shadow");
-                navbar.classList.add("bg-transparent");
+                navbar.classList.add("navbg");
             }
         });
