a9ef97baa1735c49284b62a6865c9d1afb4a703c
haval95 <havalrezan22@gmail.com>
2023-05-14 00:00:24 +0300

    FEAT: MAIN PAGES ADDED

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="home_view")
+    path("", views.index, name="home_view"),
+    path("about/", views.about, name="about_view"),
 ]
--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-# Create your views here.
-def menu (request):
+
+
+def menu(request):
     return render(request, "menu.html", {})
     
