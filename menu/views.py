3e031c0861d19c7f2def44a19c417cc4531a8daf
haval95 <havalrezan22@gmail.com>
2023-05-15 23:10:59 +0300

    FEAT: MENU page reads data from the db

--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-
+from .models import Menu_Item
 
 def menu(request):
-    
-    return render(request, "menu.html", {})
+    query_param = request.GET.get('category', None)
+    if query_param:
+        queryset = Menu_Item.objects.filter(category_id=query_param).order_by('item_name')
+    else:
+        queryset = Menu_Item.objects.all().order_by('item_name')
+    item_found = queryset.exists()
+    context = {
+        "items": queryset,
+        'item_found': item_found
+    }
+    return render(request, "menu.html", context)
     
